70e235b93d32799675e6f9822be727f4e864e367
26096412 <26096412@student.g.nwu.ac.za>
2019-10-11 15:55:13 +0200

    Login
    Created login front-end

---
+++ KalahariTickets-SPA/src/app/log/log.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log',
+  templateUrl: './log.component.html',
+  styleUrls: ['./log.component.css']
+})
+export class LogComponent implements OnInit {
+  model: any = {};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    console.log(this.model);
+  }
+
+}
