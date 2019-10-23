b2541f577af522ec8857730f136234f8e453c1d1
Dirk <odendaaldirkjnr@gmail.com>
2019-10-23 22:42:32 +0200

    Fix After map merge

--- KalahariTickets-SPA/src/app/app-routing.module.ts
+++ KalahariTickets-SPA/src/app/app-routing.module.ts
@@ -6,15 +6,12 @@ import { LoginComponent } from './login/login.component';
 import { TechnicianComponent } from './technician/technician.component';
 import { HomeComponent } from './home/home.component';
 import { AuthService } from './_services/auth.service';
-<<<<<<< HEAD
 import { MapComponent } from './map/map.component';
-=======
 import { AddClientComponent } from './add-client/add-client.component';
 import { OpenTicketComponent } from './open-ticket/open-ticket.component';
 import { EditTicketComponent } from './edit-ticket/edit-ticket.component';
 import { AddTicketComponent } from './add-ticket/add-ticket.component';
 import { OpenClientComponent } from './open-client/open-client.component';
->>>>>>> master
 
 
 
@@ -24,22 +21,11 @@ const routes: Routes = [
     { path: 'tech', component: TechnicianComponent},
     { path: 'home', component: HomeComponent},
     { path: 'login', component: LoginComponent},
-<<<<<<< HEAD
-    { path: 'map', component: MapComponent}
-=======
-    { path: 'addclient', component: AddClientComponent},
-<<<<<<< HEAD
+    { path: 'map', component: MapComponent},
     { path: 'openticket', component: OpenTicketComponent},
     { path: 'editticket', component: EditTicketComponent},
     { path: 'addticket/:id', component: AddTicketComponent},
     { path: 'client/:id', component: OpenClientComponent}
-=======
-    { path: 'openticket', component : OpenTicketComponent},
-    { path: 'editticket', component : EditTicketComponent},
-    { path: 'addticket', component : AddTicketComponent},
-    { path: 'openclient', component: OpenClientComponent}
->>>>>>> master
->>>>>>> master
   ];
 
 @NgModule({
