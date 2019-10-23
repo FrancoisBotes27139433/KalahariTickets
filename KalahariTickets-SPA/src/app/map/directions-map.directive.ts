0eaa4c866992cccdb5f12f0e91494f256a6d874b
26096412 <26096412@student.g.nwu.ac.za>
2019-10-23 19:37:55 +0200

    Google maps bouncy icon

--- KalahariTickets-SPA/src/app/map/directions-map.directive.ts
+++ KalahariTickets-SPA/src/app/map/directions-map.directive.ts
@@ -1,6 +1,7 @@
 import { Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {GoogleMapsAPIWrapper} from '@agm/core';
 import {Directive} from '@angular/core';
+import {MapComponent} from './map.component'
 
 // You can use any other interface for origin and destination, but it must contain latlng data
 export interface ILatLng {
@@ -19,7 +20,7 @@ declare var google: any;
   })
 
 export class DirectionsMapDirective implements OnInit {
-  origin: ILatLng;
+  @Input() origin: ILatLng;
   @Input() destination: ILatLng;
   @Input() showDirection: boolean;
 
